d5d6b1198d5c03d5ca21343d6ff2b7998ddac7f4
TDrumond <thiago_drumond@outlook.com.br>
2022-10-19 11:09:45 -0300

    Initial commit

---
+++ src/test/java/estudos/br/com/tekqa/WorkshopMongo/WorkshopMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package estudos.br.com.tekqa.WorkshopMongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
