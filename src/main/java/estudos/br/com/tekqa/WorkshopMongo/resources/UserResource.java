db60f8dcb0a075f35093f482e26e07a289cbc9de
TDrumond <thiago_drumond@outlook.com.br>
2022-10-25 13:52:09 -0300

    Alterando o Projeto para implementar o uso de DTO de usuario

--- src/main/java/estudos/br/com/tekqa/WorkshopMongo/resources/UserResource.java
+++ src/main/java/estudos/br/com/tekqa/WorkshopMongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package estudos.br.com.tekqa.WorkshopMongo.resources;
 
 import estudos.br.com.tekqa.WorkshopMongo.domain.User;
+import estudos.br.com.tekqa.WorkshopMongo.dto.UserDTO;
 import estudos.br.com.tekqa.WorkshopMongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -18,8 +20,9 @@ public class UserResource {
 
   // @GetMapping // Outra forma de notação.
   @RequestMapping(method = RequestMethod.GET)
-  public ResponseEntity<List<User>> findaAll() {
+  public ResponseEntity<List<UserDTO>> findaAll() {
     List<User> list = service.findAll();
-    return ResponseEntity.ok(list);
+    List<UserDTO> listDto = list.stream().map(x-> new UserDTO(x)).collect(Collectors.toList());
+    return ResponseEntity.ok(listDto);
   }
 }
