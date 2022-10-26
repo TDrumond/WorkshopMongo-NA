fb006c68cc339e79026f566ccd8c44f16b3cfcfe
TDrumond <thiago_drumond@outlook.com.br>
2022-10-26 09:41:47 -0300

    Implementação do Delete para deleção de usuário.

--- src/main/java/estudos/br/com/tekqa/WorkshopMongo/resources/UserResource.java
+++ src/main/java/estudos/br/com/tekqa/WorkshopMongo/resources/UserResource.java
@@ -43,4 +43,10 @@ public class UserResource {
             .toUri(); // Pega o endereço do novo usuario criado.
     return ResponseEntity.created((uri)).build();
   }
+
+  @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+  public ResponseEntity<Void> delete(@PathVariable String id) {
+    service.delete(id);
+    return ResponseEntity.noContent().build();
+  }
 }
