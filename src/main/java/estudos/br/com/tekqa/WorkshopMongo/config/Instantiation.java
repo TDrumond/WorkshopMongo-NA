141342e482427fc93bc97ddde4d4c9ab216c82b0
TDrumond <thiago_drumond@outlook.com.br>
2022-10-26 10:50:21 -0300

    atualização de carga de testes no banco de dados.

--- src/main/java/estudos/br/com/tekqa/WorkshopMongo/config/Instantiation.java
+++ src/main/java/estudos/br/com/tekqa/WorkshopMongo/config/Instantiation.java
@@ -1,27 +1,49 @@
 package estudos.br.com.tekqa.WorkshopMongo.config;
 
+import estudos.br.com.tekqa.WorkshopMongo.domain.Post;
 import estudos.br.com.tekqa.WorkshopMongo.domain.User;
+import estudos.br.com.tekqa.WorkshopMongo.repository.PostRepository;
 import estudos.br.com.tekqa.WorkshopMongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
 
   @Autowired private UserRepository userRepository;
+  @Autowired private PostRepository postRepository;
 
   @Override
   public void run(String... args) throws Exception {
 
+    SimpleDateFormat sdf =
+        new SimpleDateFormat("dd/MM/yyyy"); // Configurando o formato da data a ser usado.
+    sdf.setTimeZone(TimeZone.getTimeZone("GMT")); // configurando o Fuso horário adotado.
+
     userRepository.deleteAll();
+    postRepository.deleteAll();
 
     User maria = new User(null, "Maria Brown", "maria@gmail.com");
     User alex = new User(null, "Alex Green", "alex@gmail.com");
     User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
+    Post post1 =
+        new Post(
+            null,
+            sdf.parse("21/03/2018"),
+            "Partiu viagem",
+            "Vou viajar para São Paulo. Abraços!",
+            maria);
+
+    Post post2 =
+        new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei muito feliz hoje!", maria);
+
     userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    postRepository.saveAll(Arrays.asList(post1, post2));
   }
 }
