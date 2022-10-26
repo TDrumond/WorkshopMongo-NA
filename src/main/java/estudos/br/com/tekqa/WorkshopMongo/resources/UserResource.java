553535b9996b1d124337c57352eb7153d0514014
TDrumond <thiago_drumond@outlook.com.br>
2022-10-26 08:57:46 -0300

    Alterando o Projeto para implementar a busca por Id já com tratamento de erro.

--- src/main/java/estudos/br/com/tekqa/WorkshopMongo/resources/UserResource.java
+++ src/main/java/estudos/br/com/tekqa/WorkshopMongo/resources/UserResource.java
@@ -5,6 +5,7 @@ import estudos.br.com.tekqa.WorkshopMongo.dto.UserDTO;
 import estudos.br.com.tekqa.WorkshopMongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,14 +16,19 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-  @Autowired
-  private UserService service;
+  @Autowired private UserService service;
 
   // @GetMapping // Outra forma de notação.
   @RequestMapping(method = RequestMethod.GET)
   public ResponseEntity<List<UserDTO>> findaAll() {
     List<User> list = service.findAll();
-    List<UserDTO> listDto = list.stream().map(x-> new UserDTO(x)).collect(Collectors.toList());
+    List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
     return ResponseEntity.ok(listDto);
+  }
+
+  @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+  public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+    User obj = service.findById(id);
+    return ResponseEntity.ok(new UserDTO(obj));
   }
 }
--- src/main/java/estudos/br/com/tekqa/WorkshopMongo/services/UserService.java
+++ src/main/java/estudos/br/com/tekqa/WorkshopMongo/services/UserService.java
@@ -2,18 +2,24 @@ package estudos.br.com.tekqa.WorkshopMongo.services;
 
 import estudos.br.com.tekqa.WorkshopMongo.domain.User;
 import estudos.br.com.tekqa.WorkshopMongo.repository.UserRepository;
+import estudos.br.com.tekqa.WorkshopMongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
 
-    @Autowired
-    private UserRepository repo;
+  @Autowired private UserRepository repo;
 
-    public List<User> findAll(){
-        return repo.findAll();
-    }
+  public List<User> findAll() {
+    return repo.findAll();
+  }
+
+  public User findById(String id) {
+    Optional<User> obj = repo.findById(id);
+    return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+  }
 }
